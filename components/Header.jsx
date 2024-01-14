763d3f592ce0149a0bcdd205db658ef865713287
senayakagunduz <senayakgndz@gmail.com>
2024-01-14 09:53:14 +0300

    serach

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,23 @@
 "use client"
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import "../app/globals.css"
 import { BiSearch } from 'react-icons/bi'
 import MenuItem from './MenuItem'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const Header = () => {
+
+const[keyword, setKeyword] = useState("");
+const router=useRouter();
+
+const searchedItem=(e)=>{
+    if(e.key === "Enter" && keyword.length>3){
+        router.push(`/search/${encodeURIComponent(keyword)}`)
+        setKeyword("");
+    }
+}
     const menus=[
         {
             name:"Bize Yazın",
@@ -13,12 +25,19 @@ const Header = () => {
         },
        
     ]
+
     return (
         <header className='flex items-center justify-around gap-10 h-20 bg-slate-900 px-7 pt-6 '>
             <button className='hidden lg:block cursor-pointer p-3 text-xl border-2 border-yellow-400 bg-yellow-400 text-black rounded-lg hover:bg-transparent  hover:text-yellow-400 font-bold'>
                 <Link href='/'>Lets Watch Movie</Link></button>
             <div className='flex flex-1 items-center gap-2 rounded-lg bg-white p-1 cursor-pointer'>
-                <input type="text" placeholder="Arama yapınız!" className="flex-1 input cursor-pointer outline-none" />
+                <input 
+                type="text"
+                value={keyword}
+                onKeyDown={searchedItem}
+                onChange={(e)=>setKeyword(e.target.value)}
+                placeholder="Arama yapınız!" 
+                className="flex-1 input cursor-pointer outline-none" />
                 <BiSearch size={25} />
             </div>
             {
